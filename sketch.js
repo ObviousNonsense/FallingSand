fa531c3ffd5209667e5612b7fd575ce89d1cf925
ObviousNonsense <ObviousNonsense.1110@gmail.com>
2021-04-27 19:46:28 -0400

    Mouse click to add sand. Add walls - makes sand logic much simpler

--- sketch.js
+++ sketch.js
@@ -18,12 +18,17 @@ function setup() {
 	fr = createP('');
 	frHistory = new Array(60);
 
-	for (let i = 0; i < gridWidth; i++) {
-		grid[i] = [];
-		nextGrid[i] = [];
-		for (let j = 0; j < gridHeight; j++) {
-			grid[i][j] = false;
-			nextGrid[i][j] = false;
+	for (let x = 0; x < gridWidth; x++) {
+		grid[x] = [];
+		nextGrid[x] = [];
+		for (let y = 0; y < gridHeight; y++) {
+			if (y === gridHeight - 1 || x === 0 || x === gridWidth - 1) {
+				grid[x][y] = new WallParticle(x, y);
+			}
+			else {
+				grid[x][y] = false;
+			}
+			nextGrid[x][y] = false;
 		}
 	}
 
@@ -34,8 +39,18 @@ function setup() {
 
 function draw() {
 	background(0);
-	new SandParticle(2, 0);
-	new SandParticle(75, 50);
+	// new SandParticle(2, 0);
+	// new SandParticle(75, 50);
+
+	if (mouseIsPressed) {
+		if (mouseX < width && mouseX >= 0 && mouseY < height && mouseY >= 0) {
+			let x = floor(mouseX / pixelsPerParticle);
+			let y = floor(mouseY / pixelsPerParticle);
+			if (!grid[x][y]){
+				new SandParticle(x, y);
+			}
+		}
+	}
 
 	for (let j = gridHeight - 1; j >= 0; j--) {
 		for (let i = 0; i < gridWidth; i++) {
@@ -55,21 +70,41 @@ function draw() {
 	nextGrid = temp;
 
 	fr.html(floor(averageFrameRate()));
-    // noLoop();
+	// noLoop();
 
-    function averageFrameRate() {
-        frHistory[frHistoryIndex] = frameRate();
-        frHistoryIndex += 1;
-        if (frHistoryIndex >= frHistory.length) {
-            frHistoryIndex = 0;
-        }
+	function averageFrameRate() {
+		frHistory[frHistoryIndex] = frameRate();
+		frHistoryIndex += 1;
+		if (frHistoryIndex >= frHistory.length) {
+			frHistoryIndex = 0;
+		}
 
-        sum = 0;
-        for (var i = 0; i < frHistory.length; i++) {
-            sum += frHistory[i];
-        }
-        return sum/frHistory.length;
-    }
+		sum = 0;
+		for (var i = 0; i < frHistory.length; i++) {
+			sum += frHistory[i];
+		}
+		return sum / frHistory.length;
+	}
+}
+
+
+function WallParticle(i, j) {
+	this.row = j;
+	this.col = i;
+	grid[i][j] = this;
+
+	this.show = function () {
+		noStroke();
+		fill(65, 68, 74);
+		rect(this.col * pixelsPerParticle,
+			this.row * pixelsPerParticle,
+			pixelsPerParticle,
+			pixelsPerParticle);
+	}
+
+	this.update = function () {
+		nextGrid[this.col][this.row] = this;
+	}
 }
 
 
@@ -88,22 +123,20 @@ function SandParticle(i, j) {
 	}
 
 	this.update = function () {
-		if (this.row + 1 < gridHeight) {
-			if (!nextGrid[this.col][this.row + 1]) {
-				// console.log(this.col + ", " + this.row);
-				this.updateGridPosition(this.col, this.row + 1);
-			}
-			else if (this.col - 1 >= 0 && !nextGrid[this.col - 1][this.row + 1]) {
-				this.updateGridPosition(this.col - 1, this.row + 1);
-			}
-			else if (this.col + 1 < gridWidth && !nextGrid[this.col + 1][this.row + 1]) {
-				this.updateGridPosition(this.col + 1, this.row + 1);
-			}
-			else {
-				nextGrid[this.col][this.row] = this;
-			}
+		if (!nextGrid[this.col][this.row + 1]) {
+			// Move straight down
+			this.updateGridPosition(this.col, this.row + 1);
+		}
+		else if (!nextGrid[this.col - 1][this.row + 1]) {
+			// Move down and left
+			this.updateGridPosition(this.col - 1, this.row + 1);
+		}
+		else if (!nextGrid[this.col + 1][this.row + 1]) {
+			// Move down and right
+			this.updateGridPosition(this.col + 1, this.row + 1);
 		}
 		else {
+			// Don't move
 			nextGrid[this.col][this.row] = this;
 		}
 	}
